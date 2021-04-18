a1cd382f7f52611fcd4ee3f32aebb59bc5daff9c
BuonsantoVito <vito.buonsanto@dxc.com>
2021-04-18 18:14:43 +0200

    fix

--- src/app/services/download.service.ts
+++ src/app/services/download.service.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
+import infoGenerali from 'src/assets/files/infoGenerali.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DownloadService {
-
-  constructor() { }
+  candidato;
+  infoGenerali: any;
+  constructor() {
+    const json = infoGenerali.value;
+    this.infoGenerali = json;
+    this.candidato = this.infoGenerali.nomeCandidato;
+   }
 
   downloadCurriculum(filePath) {
     const link = document.createElement('a');
     link.setAttribute('type', 'hidden');
     link.href = filePath;
-    link.download = 'CV_Almalaurea.pdf';
+    link.download = 'Curriculum_' + this.candidato + '.pdf';
     document.body.appendChild(link);
     link.click();
     link.remove();
